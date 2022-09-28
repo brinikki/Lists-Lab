a9697af413dcd270ea8734e87506ec9847b144d1
ieee754 <94340126+brinikki@users.noreply.github.com>
2022-09-28 13:19:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+my_list = []
+for number in range(3):
+  intinput = int(input("Provide a integer: "))
+  my_list.append(intinput)
+  my_list.append(intinput**2)
+  print (my_list)
+  print("\n")
+for loop in range(1):
+  print('The first item of the list:', my_list[0])
+  print('The second item of the list:', my_list[2])
+  print('The third item of the list:', my_list[4])
